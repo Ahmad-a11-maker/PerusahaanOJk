54faad0b6098f3323b6fce65a71dbe648d986c0c
Ahmad Fauzan <81544080+Ahmad-a11-maker@users.noreply.github.com>
2022-10-16 23:48:39 +0700

    Add files via upload

---
+++ ojk.php
@@ -0,0 +1,43 @@
+<?php
+error_reporting (E_ALL ^ E_WARNING||E_NOTICE);
+$data = file_get_contents("https://ojk-invest-api.vercel.app/api/illegals");
+$ojk =json_decode($data, true);
+
+//echo "<pre>";print_r($ojk);
+
+$table = "<h3>Data Perusahaan OJK</h3>";
+$table .= "<table border=1>
+			<tr><td>No</td>
+			    <td>Nama PT</td>
+				<td>Alamat</td>
+				<td>Telephone</td>
+				<td>Tipe</td>
+				<td>Web</td></tr>";
+for($i=0;$i<count($ojk["data"]["illegals"]);$i++){
+
+	if (empty(["data"]["illegals"][$i]["number"])){
+		$display_number = null;
+	}else
+		for($p=0; $p<count($ojk["data"]["illegals"][$i]["number"]);$p++){
+		$display_number =$ojk["data"]["illegals"][$i]["number"][$p];
+	}
+
+	if (empty(["data"]["illegals"][$i]["urls"])){
+		$display_urls = null;
+	}else
+		for($z=0; $p<count($ojk["data"]["illegals"][$i]["urls"]);$z++){
+		$display_urls =$ojk["data"]["illegals"][$i]["urls"][$z];
+	}
+
+	$no=$i+1;
+	$table .= "<tr><td>{$no}</td>
+			    <td>{$ojk["data"]["illegals"][$i]["name"]}</td>
+				<td>{$ojk["data"]["illegals"][$i]["address"]}</td>
+				<td>{$ojk["data"]["illegals"][$i]["number"][0]}</td>
+				<td>{$ojk["data"]["illegals"][$i]["type"]}</td>
+				<td>{$ojk["data"]["illegals"][$i]["urls"][0]}</td>
+				</tr>";
+}
+$table .= "</table>";
+echo $table;				
+?>
